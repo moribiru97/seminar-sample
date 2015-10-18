70be036193cba2db6a4c157b7c12e8654b990323
Yusuke Mori <yusuke.mori@o-05448-mac.local>
2015-10-18 19:22:28 +0900

    jquery.tmplを使うように修正

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 (function(global){
     var seminorInfo = {};
     //初期化
-    seminorInfo.init = function() {
+    seminorInfo.init = function(options) {
+        options |= {};
+        var rankingTmpl = options.rankingTmpl || "<div class=\"${rank}\">${rank}位：${seminor.name}</div>";
+        var themesTmpl = options.themesTmpl || "<div id=\"${theme.id}\" class=\"${theme.id}\"><h3>${theme.title}</h3></div>";
+        var themeSeminorTmpl = options.themeSeminorTmpl || "<div class=\"${theme.id}_${seminor.id}\">${seminor.name}</div>";
+        var calTmpl = options.calTmpl || "<span class=\"title\"><a href=\"${seminor.detail}\" id=\"pop${seminor.id}\">${seminor.name}</a></span>";
+        var calTipTmpl = options.calTipTmpl || "<span>${seminor.name}<br>日時：${seminor.date}<br>開催会社：${seminor.corp}<br>最寄駅：${seminor.nearSta";
+
         console.log("call init");
         //全seminor情報とランキング、テーマ別用のjsonをセットする
         $.getJSON("data/seminors.json", function(data) {
@@ -17,48 +24,59 @@
             seminorInfo.seminors = _seminors; 
             console.log("done init");
             //initが終わり次第各種表示を呼び出す
-            seminorInfo.showRanking();
-            seminorInfo.showThemes();
-            seminorInfo.showCalender();
+            seminorInfo.showRanking(rankingTmpl);
+            seminorInfo.showThemes(themesTmpl, themeSeminorTmpl);
+            seminorInfo.showCalender(calTmpl, calTipTmpl);
         });
     };
 
     //ランキング表示
-    seminorInfo.showRanking = function() {
+    seminorInfo.showRanking = function(template) {
         $.each(seminorInfo.ranking, function(index, rankSeminorId){
             //jsonのrankingで設定したseminorのidを使ってseminorを取得
-            var rankSeminor = seminorInfo.seminors[rankSeminorId];
+            var seminor = seminorInfo.seminors[rankSeminorId];
             //それっぽいところにappend.indexは0からなので+1すると順位
             var rank = index + 1;
-            $("#ranking").append("<div class=\"\"" + rank + ">" + rank + "位：" + rankSeminor.name + "</div>");
+            $.tmpl(template,{
+                rank: rank,
+                seminor: seminor
+            }).appendTo("#ranking");
         });
     };
 
     //テーマ別表示
-    seminorInfo.showThemes = function() {
+    seminorInfo.showThemes = function(themesTmpl, themeSeminorTmpl) {
         //全テーマを持って来てroop
-        var themesEl = $("#themes");
         $.each(seminorInfo.themes, function(index, theme){
-            themesEl.append("<div id=\"" + theme.id + "\" class=\"" + theme.id + "\"><h3>" + theme.title + "</h3></div>");
+            var themesEl = $("#themes");
+            $.tmpl(themesTmpl, {
+                theme: theme
+            }).appendTo(themesEl);
             $.each(theme.seminors, function(index, themeSeminorId){
-                var themeSeminor = seminorInfo.seminors[themeSeminorId];
+                var seminor = seminorInfo.seminors[themeSeminorId];
                 //各テーマのdivに当該セミナーをappend
-                var tEl = themesEl.children("div#" + theme.id);
-                tEl.append("<div class=\"\"" + theme.id + "_" + themeSeminorId + ">" + themeSeminor.name + "</div>");
+                $.tmpl(themeSeminorTmpl,{
+                    theme: theme,
+                    seminor: seminor
+                }).appendTo(themesEl.children("div#" + theme.id));
             });
         });
     };
 
-    seminorInfo.showCalender = function() {
+    seminorInfo.showCalender = function(calTmpl, calTipTmpl) {
         console.log("call showCal");
         //全セミナーをループで回す
         $.each(seminorInfo.seminors, function(index,seminor){
             //seminorのdateを使って、あらかじめidを振っておいたcalendarのtdを取得し、そこにappend
             //pop用のidも振っておく
-           $("#day_" + seminor.date.getDate()).append("<span class=\"title\"><a href=\"" + seminor.detail + "\" id=\"pop" + seminor.id + "\">" + seminor.name + "</a></span>"); 
+            $.tmpl(calTmpl, {
+               seminor: seminor
+            }).appendTo($("#day_" + seminor.date.getDate()));
             //tooltip（既存サイトにあったので。）
             $('#pop' + seminor.id).tooltipster({
-                content: $('<span>' + seminor.name + '<br>日時：' + seminor.date + '<br>開催会社：' + seminor.corp + '<br>最寄駅：' + seminor.nearSta)
+                content: $.tmpl(calTipTmpl, {
+                    seminor: seminor
+                })
             });
         });
     };
