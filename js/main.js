b4a8ca99a201084c337183ff164fb02ff47377ea
yusuke.mori <yusuke.mori@dena.com>
2015-10-21 22:53:51 +0900

    会社別表示対応。

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
 (function(global){
     var seminorInfo = {};
+    function dateFormat(date) {
+        var y = date.getFullYear();
+        var m = date.getMonth() + 1;
+        var d = date.getDate();
+        var w = date.getDay();
+        var wNames = ['日', '月', '火', '水', '木', '金', '土'];
+
+        if (m < 10) {
+            m = '0' + m;
+        }
+        if (d < 10) {
+            d = '0' + d;
+        }
+        return y + '年' + m + '月' + d + '日 (' + wNames[w] + ')';
+    }
     //初期化
     seminorInfo.init = function(options, cb) {
         if (!cb ) {
             throw "init callback is undifined";
         }
-        var _targetCorpName = options.targetCorpName || null;
+        seminorInfo.targetCorpId = options.targetCorpId || null;
         var _cb = cb;
         //全seminor情報とランキング、テーマ別用のjsonをセットする
         $.getJSON("data/seminars.json", function(data) {
-            seminorInfo.ranking = data.ranking; 
+            var _corps = {};
+            $.each(data.corps, function(i,corp) {
+                if (seminorInfo.targetCorpId && corp.id !== seminorInfo.targetCorpId) {
+                    return;
+                }
+                _corps[corp.id] = corp;
+            });
+            seminorInfo.corps   = _corps;
+            seminorInfo.ranking = seminorInfo.targetCorpId ? data.ranking[seminorInfo.targetCorpId] : data.ranking["all"]; 
             seminorInfo.themes  = data.themes;
             var _seminors = {};
             $.each(data.seminors, function(i,seminor) {
                 //tagetCorpNameが指定されていたらそれ以外の会社を除外
-                if (_targetCorpName && seminor.corp !== _targetCorpName) {
+                if (seminorInfo.targetCorpId && seminor.corpId !== seminorInfo.targetCorpId) {
                     return;
                 }
                 //dateをparseして詰め直す
                 var d_ary = seminor.date.split("/");
-                seminor.date = new Date(parseInt(d_ary[0]),parseInt(d_ary[1]),parseInt(d_ary[2]));
+                seminor._date = new Date(parseInt(d_ary[0]),parseInt(d_ary[1]),parseInt(d_ary[2]));
+                seminor.date = dateFormat(seminor._date);
+                seminor.corp = seminorInfo.corps[seminor.corpId].name;
+                seminor.detail = seminorInfo.corps[seminor.corpId].detail;
                 _seminors[seminor.id] = seminor;
             });
             seminorInfo.seminors = _seminors; 
@@ -51,13 +77,14 @@
         var themesTmpl = params.themesTmpl;
         var themeSeminorTmpl = params.themeSeminorTmpl;
         //全テーマを持って来てroop
-        $.each(seminorInfo.themes, function(index, theme){
+        $.each(seminorInfo.themes, function(index, theme) {
             var themesEl = $("#" + elmId);
             $.tmpl(themesTmpl, {
                 theme: theme
             }).appendTo(themesEl);
             $.each(theme.seminors, function(index, themeSeminorId){
                 var seminor = seminorInfo.seminors[themeSeminorId];
+                if (!seminor) return;
                 //各テーマのdivに当該セミナーをappend
                 $.tmpl(themeSeminorTmpl,{
                     theme: theme,
@@ -80,7 +107,7 @@
             //pop用のidも振っておく
             $.tmpl(calTmpl, {
                seminor: seminor
-            }).appendTo($("#" + elmIdPrefix + seminor.date.getDate()));
+            }).appendTo($("#" + elmIdPrefix + seminor._date.getDate()));
             //tooltip（既存サイトにあったので。）
 
             var ctId = $.tmpl(calTipElmId, {
